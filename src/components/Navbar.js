767f94938394338d516b8ef58d4ebf2a3cf188ea
SriramCSB057 <146874704+SriramCSB057@users.noreply.github.com>
2025-12-26 12:12:30 +0530

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { Link } from "react-scroll";
+
+export default function MyNavbar() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+      <Container>
+
+        <Navbar.Brand className="fw-bold fs-3">Sriram</Navbar.Brand>
+        <Navbar.Toggle />
+
+        <Navbar.Collapse>
+          <Nav className="ms-auto gap-3 fs-5">
+
+            <Link to="home" smooth duration={500} className="nav-link">Home</Link>
+            <Link to="about" smooth duration={500} className="nav-link">About</Link>
+            <Link to="skills" smooth duration={500} className="nav-link">Skills</Link>
+            <Link to="projects" smooth duration={500} className="nav-link">Projects</Link>
+            <Link to="contact" smooth duration={500} className="nav-link">Contact</Link>
+
+            <a
+              className="nav-link"
+           href={process.env.PUBLIC_URL + "/Sriram_Java_Full_stack_Resume.pdf"}
+              download
+            >
+              Resume
+            </a>
+
+          </Nav>
+        </Navbar.Collapse>
+
+      </Container>
+    </Navbar>
+  );
+}
